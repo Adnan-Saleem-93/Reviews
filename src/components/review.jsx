cad66e37aec382ff3f9190e81ed0b512765f6de5
Adnan Saleem <adnan13893@gmail.com>
2021-06-24 11:09:05 +0500

    some css changes

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -4,7 +4,7 @@ import {reviews} from "../utils/constants";
 const Review = () => {
   const [data, setData] = useState(reviews);
 
-  return <div className="review"></div>;
+  return <div className="review">Review</div>;
 };
 
 export default Review;
