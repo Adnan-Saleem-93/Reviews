e27662ee755ccf17d1367c3c3b79a4dc074a4b71
Adnan Saleem <adnan13893@gmail.com>
2021-06-25 11:01:43 +0500

    css changes and setting up review section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <h1>Reviews</h1>
+      <hr
+        style={{
+          width: "10rem",
+          height: "5px",
+          color: "dodgerblue",
+          marginBottom: "2rem",
+          marginTop: "0",
+          opacity: 1
+        }}
+      />
       <Review />
     </div>
   );
--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,10 +1,20 @@
 import React, {useState} from "react";
 import {reviews} from "../utils/constants";
+import {Image} from "react-bootstrap";
 
 const Review = () => {
-  const [data, setData] = useState(reviews);
+  const [data, setData] = useState(reviews[0]);
 
-  return <div className="review">Review</div>;
+  return (
+    <div className="review">
+      <div className="img-section">
+        <Image src={data.image} roundedCircle />
+      </div>
+      <h5 className="name">{data.name}</h5>
+      <p className="job">{data.job}</p>
+      <p>{data.text}</p>
+    </div>
+  );
 };
 
 export default Review;
