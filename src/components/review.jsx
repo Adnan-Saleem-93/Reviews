7be6db54cad3f8394571c60216f5aa6a17d61ab4
Adnan Saleem <adnan13893@gmail.com>
2021-06-29 12:07:56 +0500

    added 'surprise me' button and functionality + css

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {reviews} from "../utils/constants";
-import {Image} from "react-bootstrap";
+import {Image, Button} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowAltCircleLeft, faArrowAltCircleRight} from "@fortawesome/free-solid-svg-icons";
 
@@ -13,13 +13,34 @@ const Review = () => {
     setCurrentReview(data[index]);
   }, [index]);
 
+  // handle click of the arrow buttons
   const handleArrowClick = (direction) => {
-    // console.log(direction);
+    // if left arrow is clicked, decrease the index
     if (direction === "left") {
       index === 0 ? setIndex(data.length - 1) : setIndex(index - 1);
-    } else {
+    }
+    // if right arrow is clicked, increase the index
+    else {
       index === data.length - 1 ? setIndex(0) : setIndex(index + 1);
     }
+  };
+
+  // this method will be used to select a random review from the 'data' array
+  const handleSurpriseMeClick = () => {
+    let indexes = [];
+    // get all indexes
+    data.map((item, index) => {
+      indexes.push(index);
+    });
+
+    // remove the index of current review from indexes array, to avoid selection of that index again
+    let filtered_indexes = indexes.filter((value, ind) => value !== index);
+
+    // get a random index value from the indexes array
+    var random = filtered_indexes[Math.floor(Math.random() * filtered_indexes.length)];
+
+    // set the random value as new index
+    setIndex(random);
   };
 
   return (
@@ -38,6 +59,7 @@ const Review = () => {
           <FontAwesomeIcon icon={faArrowAltCircleRight} onClick={() => handleArrowClick("right")} />
         </button>
       </div>
+      <Button onClick={handleSurpriseMeClick}>Surprise Me</Button>
     </div>
   );
 };
